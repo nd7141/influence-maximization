b4d82e05eda0889233a2964dc427e6a3b7939ca3
Ivanov Sergey <ivanovserg990@gmail.com>
2014-02-20 17:24:27 +0400

    Implement representativeNodes heuristic with using priority queues

---
+++ priorityQueue.py
@@ -0,0 +1,38 @@
+__author__ = 'http://docs.python.org/2/library/heapq.html#priority-queue-implementation-notes'
+
+import itertools
+from heapq import *
+
+class PriorityQueue(object):
+    def __init__(self):
+        self.pq = []                         # list of entries arranged in a heap
+        self.entry_finder = {}               # mapping of tasks to entries
+        self.REMOVED = '<removed-task>'      # placeholder for a removed task
+        self.counter = itertools.count()     # unique sequence count
+
+    def add_task(self, task, priority=0):
+        'Add a new task or update the priority of an existing task'
+        if task in self.entry_finder:
+            self.remove_task(task)
+        count = next(self.counter)
+        entry = [priority, count, task]
+        self.entry_finder[task] = entry
+        heappush(self.pq, entry)
+
+    def remove_task(self, task):
+        'Mark an existing task as REMOVED.  Raise KeyError if not found.'
+        entry = self.entry_finder.pop(task)
+        entry[-1] = self.REMOVED
+
+    def pop_item(self):
+        'Remove and return the lowest priority task. Raise KeyError if empty.'
+        while self.pq:
+            priority, count, task = heappop(self.pq)
+            if task is not self.REMOVED:
+                del self.entry_finder[task]
+                return priority, task
+        raise KeyError('pop from an empty priority queue')
+
+
+if __name__ == '_main__':
+    console = []
---
+++ representativeNodes.py
@@ -0,0 +1,117 @@
+__author__ = 'ivanovsergey'
+
+from priorityQueue import PriorityQueue as PQ # priority queue class
+from IC import runIC
+import networkx as nx
+import matplotlib.pylab as plt
+
+
+def _sumDist (G, S, no):
+    ''' Compute cumulative distance from node to set S
+    '''
+    cum = 0
+    for u in S:
+        try:
+            cum += G[no][u]['weight']
+        except:
+            pass
+    return cum
+
+def _minDist (G, S, no):
+    ''' Compute minimal distance from node to S
+    '''
+    min_dist = float('Inf')
+    for u in S:
+        try:
+            min_dist = min(G[no][u]['weight'], min_dist)
+        except:
+            pass
+    if min_dist == float('Inf'):
+        return -1
+    else:
+        return min_dist
+
+def representativeNodes(G, k, metric=1):
+    ''' Finds the most distinguishable (representative) nodes in graph G greedily.
+    Takes the most furthest node to the already chosen nodes at each step.
+
+    Input: G -- networkx object graph with weighted edges
+    k -- number of nodes needed
+    metric -- parameter for differentiating representative qualities
+    metric == 1 trying to maximize total distance in the chosen set of k nodes
+    metric == 2 trying to maximize minimal distance between a pair of k nodes
+    Output:
+    S -- chosen k nodes
+    objv -- objective value according to the chosen metric and set of nodes
+    '''
+
+    S = [] # set of chosen nodes
+    S_dist = PQ() # distances from each node in G to set S according to metric
+
+    # initialize S with furthest vertices
+    try:
+        u,v,d = max(G.edges(data=True), key=lambda (u, v, d): d['weight'])
+    except KeyError:
+        raise KeyError, 'Most likely you have no weight attribute'
+    S.extend([u,v])
+
+    # compute distances from each node in G to S
+    for v in G.nodes():
+        if v not in S: # calculate only for nodes in G
+            if metric == 1:
+                S_dist.add_task(v, - _sumDist(G, S, v)) # take minus to pop the maximum value from priority queue
+            elif metric == 2:
+                S_dist.add_task(v, - _minDist(G, S, v)) # take minus to pop the maximum value from priority queue
+
+    # add new nodes to the set greedily
+    while len(S) < k:
+        priority, u = S_dist.pop_item() # find maximum value of distance to set S
+        S.append(u) # append that node to S
+
+        # only increase distance for nodes that are connected to u
+        for v in G[u].keys():
+            if v not in S: # add only remained nodes
+                [priority, count, task] = S_dist.entry_finder[v] # finds distance for the previous step
+                try:
+                    if metric == 1:
+                        S_dist.add_task(v, priority-G[u][v]['weight']) # adds distance to the new member of S
+                    elif metric == 2:
+                        S_dist.add_task(v, max(priority, -G[u][v]['weight'])) # update min distance to the set S
+                except:
+                    raise u,v, "These are vertices that caused the problem"
+
+    # extract objective value of the chosen set
+    if metric == 1:
+        objv = 0
+        for u in S:
+            objv += _sumDist(G, S, u)
+    elif metric == 2:
+        objv = float('Inf')
+        for u in S:
+            objv = min(objv, _minDist(G, S, u))
+
+    return S, objv
+
+if __name__ == '__main__':
+    import time
+    start = time.time()
+
+    G = nx.Graph()
+    with open('graphdata/hep.txt') as f:
+        n, m = f.readline().split()
+        for line in f:
+            u, v = map(int, line.split())
+            try:
+                G[u][v]['weight'] += 1
+            except:
+                G.add_edge(u,v, weight=1)
+    print time.time() - start
+
+    S, objv = representativeNodes(G, 200)
+    print time.time() - start
+
+    T = runIC(G, S)
+    print time.time() - start
+    print len(T)
+
+    console = []
