26f61f24fa5fbd2b18a2020b51b5c66783ffe206
Ivanov Sergey <ivanovserg990@gmail.com>
2014-02-28 19:11:29 +0400

    Clean up test.py

--- test.py
+++ test.py
@@ -2,16 +2,24 @@
 '''
 __author__ = 'ivanovsergey'
 
-from IC import runIC
+from IC import runIC, runIC2
 from representativeNodes import representativeNodes
-from degreeDiscount import degreeDiscountIC
+from degreeDiscount import degreeDiscountIC, degreeDiscountIC2
+from singleDiscount import singleDiscount
+from generalGreedy import generalGreedy
+from newGreedyIC import newGreedyIC
+from degreeHeuristic import degreeHeuristic
+from randomHeuristic import randomHeuristic
 import networkx as nx
 import matplotlib.pylab as plt
+import multiprocessing # for parallelizing IC running
+from itertools import repeat, izip
 
 if __name__ == '__main__':
     import time
     start = time.time()
 
+    # read in graph
     G = nx.Graph()
     with open('graphdata/hep.txt') as f:
         n, m = f.readline().split()
@@ -21,12 +29,23 @@ if __name__ == '__main__':
                 G[u][v]['weight'] += 1
             except:
                 G.add_edge(u,v, weight=1)
+            # G.add_edge(u, v, weight=1)
     print time.time() - start
 
-    S = degreeDiscountIC(G, int(len(G)/100))
-    print 'Initial set of', int(len(G)/100), 'nodes'
+    #calculate initial set
+    seed_size = 30
+    S = degreeDiscountIC(G, seed_size, p=.1)
+    print 'Initial set of', seed_size, 'nodes chosen'
     print time.time() - start
-    T = runIC(G, S)
-    print 'Targeted', len(T), 'nodes out of', len(G)
+
+    # calculate average activated set size
+    iterations = 200 # number of iterations
+    avg = 0
+    for i in range(iterations):
+        T = runIC(G, S)
+        avg += float(len(T))/iterations
+        # print i, 'iteration of IC'
+    print 'Avg. Targeted', int(round(avg)), 'nodes out of', len(G)
+    print time.time() - start
 
     console = []
