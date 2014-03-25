490ef3269916895515113e441d92e782b1a9c486
Ivanov Sergey <ivanovserg990@gmail.com>
2014-03-25 11:25:45 +0400

    Add degreeHeuristic2

--- degreeHeuristic.py
+++ degreeHeuristic.py
@@ -19,8 +19,29 @@ def degreeHeuristic(G, k, p=.01):
     d = PQ()
     for u in G:
         degree = sum([G[u][v]['weight'] for v in G[u]])
+        # degree = len(G[u])
         d.add_task(u, -degree)
     for i in range(k):
         u, priority = d.pop_item()
+        S.append(u)
+    return S
+
+def degreeHeuristic2(G, k, p=.01):
+    ''' Finds initial set of nodes to propagate in Independent Cascade model (without priority queue)
+    Input: G -- networkx graph object
+    k -- number of nodes needed
+    p -- propagation probability
+    Output:
+    S -- chosen k nodes
+    '''
+    S = []
+    d = dict()
+    for u in G:
+        degree = sum([G[u][v]['weight'] for v in G[u]])
+        # degree = len(G[u])
+        d[u] = degree
+    for i in range(k):
+        u, degree = max(d.iteritems(), key=lambda (k,v): v)
+        d.pop(u)
         S.append(u)
     return S
