5baa3ac76b6589ec4efce5b1c4e44714c9597c3f
Ivanov Sergey <ivanovserg990@gmail.com>
2014-03-25 11:26:46 +0400

    Add inner timing calculation

--- generalGreedy.py
+++ generalGreedy.py
@@ -7,14 +7,16 @@ __author__ = 'ivanovsergey'
 from priorityQueue import PriorityQueue as PQ
 from IC import runIC
 
-def generalGreedy(G, k):
+def generalGreedy(G, k, p=.01):
     ''' Finds initial seed set S using general greedy heuristic
     Input: G -- networkx Graph object
     k -- number of initial nodes needed
     p -- propagation probability
     Output: S -- initial set of k nodes to propagate
     '''
-    R = 100 # number of times to run Random Cascade
+    import time
+    start = time.time()
+    R = 20 # number of times to run Random Cascade
     S = [] # set of selected nodes
     # add node to S if achieves maximum propagation for current chosen + this node
     for i in range(k):
@@ -24,7 +26,8 @@ def generalGreedy(G, k):
                 s.add_task(v, 0) # initialize spread value
                 for j in range(R): # run R times Random Cascade
                     [priority, count, task] = s.entry_finder[v]
-                    s.add_task(v, priority - float(len(runIC(G, S + [v])))/R) # add normalized spread value
+                    s.add_task(v, priority - float(len(runIC(G, S + [v], p)))/R) # add normalized spread value
         task, priority = s.pop_item()
         S.append(task)
+        print i, k, time.time() - start
     return S
