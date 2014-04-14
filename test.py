b7ff0248db7b6a00fd32c304e71ccd5634c1c156
ivanovsergey <ivanovserg990@gmail.com>
2014-04-14 18:16:01 +0400

    Save spread to visualisation file

--- test.py
+++ test.py
@@ -4,16 +4,17 @@ __author__ = 'ivanovsergey'
 
 from IC import runIC, runIC2
 from representativeNodes import representativeNodes
-from degreeDiscount import degreeDiscountIC, degreeDiscountIC2
+from degreeDiscount import degreeDiscountIC, degreeDiscountIC2, degreeDiscountStar
 from singleDiscount import singleDiscount
 from generalGreedy import generalGreedy
 from newGreedyIC import newGreedyIC
 from degreeHeuristic import degreeHeuristic
 from randomHeuristic import randomHeuristic
 import networkx as nx
-import matplotlib.pylab as plt
+#import matplotlib.pylab as plt
 import multiprocessing # for parallelizing IC running
 from itertools import repeat, izip
+import os
 
 if __name__ == '__main__':
     import time
@@ -30,16 +31,22 @@ if __name__ == '__main__':
             except:
                 G.add_edge(u,v, weight=1)
             # G.add_edge(u, v, weight=1)
+    print 'Built graph G'
     print time.time() - start
 
     #calculate initial set
-    seed_size = 50
+    seed_size = 5
     S = degreeDiscountIC(G, seed_size)
     print 'Initial set of', seed_size, 'nodes chosen'
     print time.time() - start
 
+    # write results S to file
+    with open('visualisation.txt', 'w') as f:
+        for node in S:
+            f.write(str(node) + os.linesep)
+
     # calculate average activated set size
-    iterations = 1000 # number of iterations
+    iterations = 200 # number of iterations
     avg = 0
     for i in range(iterations):
         T = runIC(G, S)
