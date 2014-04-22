21daf25a9a7ba210f238192ca0cc7dbf1f183929
ivanovsergey <ivanovserg990@gmail.com>
2014-04-22 10:45:48 +0400

    Add randomWeights, checkWC functions

--- WC/WC.py
+++ WC/WC.py
@@ -11,6 +11,30 @@ def uniformWeights(G):
             E[(v1,v2)] = 1/dv
     return E
 
+def randomWeights(G):
+    E = dict()
+    for u in G:
+        in_edges = G.in_edges([u], data=True)
+        ew = [random.random() for e in in_edges] # random edge weights
+        total = 0 # total sum of weights of incoming edges (for normalization)
+        for num, (v1, v2, edata) in enumerate(in_edges):
+            total += edata['weight']*ew[num]
+        for num, (v1, v2, _) in enumerate(in_edges):
+            E[(v1,v2)] = ew[num]/total
+    return E
+
+def checkWC(G, E, eps = 1e-4):
+    ''' To verify that sum of all incoming weights <= 1
+    '''
+    for u in G:
+        in_edges = G.in_edges([u], data=True)
+        total = 0
+        for (v1, v2, edata) in in_edges:
+            total += E[(v1, v2)]*G[v1][v2]['weight']
+        if total >= 1 + eps:
+            raise 'For node %s WC property is incorrect. Sum equals to %s' %(u, total)
+    return 'Everything is correct'
+
 def runWC(G, S, E):
 
     T = deepcopy(S) # targeted set
