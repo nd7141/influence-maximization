65887d4faed1d9aa8c1895df9c1f4216bdba39bf
ivanovsergey <ivanovserg990@gmail.com>
2014-04-24 09:34:27 +0400

    Add test of topological order

--- LT/test_LDAG.py
+++ LT/test_LDAG.py
@@ -1,1 +1,20 @@
-__author__ = 'sergey'
+
+import unittest
+from LDAG import tsort
+import networkx as nx
+
+class TestTopologicalSort(unittest.TestCase):
+    def test_sorting(self):
+        D = nx.DiGraph()
+        D.add_edge(1, 0, weight=1)
+        D.add_edge(3, 0, weight=1)
+        D.add_edge(3, 1, weight=1)
+        D.add_edge(4, 1, weight=1)
+        D.add_edge(4, 3, weight=1)
+        D.add_edge(5, 3, weight=1)
+
+        self.assertEqual(tsort(D, 0), [0,1,3,4,5])
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
+
