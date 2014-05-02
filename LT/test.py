ebf7dba733d8df997bf06c3d76a48cf74ad5bb3a
ivanovsergey <ivanovserg990@gmail.com>
2014-05-02 15:55:52 +0400

    Update running procedure for LDAG algorithm

--- LT/test.py
+++ LT/test.py
@@ -11,33 +11,6 @@ if __name__ == "__main__":
 
     start = time.time()
 
-    # G = nx.DiGraph()
-    # G.add_edge(1,2,weight=1)
-    # G.add_edge(3,2,weight=3)
-    # G.add_edge(2,3,weight=1)
-    # G.add_edge(3,3,weight=1)
-    # G.add_edge(4,5,weight=5)
-    #
-    # with open('../graphdata/graph30.txt') as f:
-    #     n, m = map(int, f.readline().split())
-    #     G = nx.DiGraph()
-    #     for line in f:
-    #         u, v = map(int, line.split())
-    #         try:
-    #             G[u][v]['weight'] += 1
-    #         except KeyError:
-    #             G.add_edge(u,v,weight=1)
-    #         try:
-    #             G[v][u]['weight'] += 1
-    #         except KeyError:
-    #             G.add_edge(v,u,weight=1)
-    # print 'Built Graph G'
-    # print time.time() - start
-
-    # Ewu = uniformWeights(G)
-    # Ewr = randomWeights(G)
-    # S = [0, 7, 11, 17, 24]
-
     with open('../graphdata/hep.txt') as f:
         n, m = map(int, f.readline().split())
         G = nx.DiGraph()
