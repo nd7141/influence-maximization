5c26663ab9191108cb287cd454d860f18f01f078
ivanovsergey <ivanovserg990@gmail.com>
2014-06-13 00:38:20 +0400

    Fix a bug for map function

--- IC/ArbitraryP/CCWP.py
+++ IC/ArbitraryP/CCWP.py
@@ -1,5 +1,4 @@
 
-#TODO implement CCWP for arbitrary probabilities using CCparallel and test_in_parallel.py
 '''
 CCWP heuristic for arbitrary propagation probabilities.
 '''
@@ -52,9 +51,10 @@ def CCWP(G, k, Ep):
     sortedCC = sorted([(len(dv), dk) for (dk, dv) in CC.iteritems()], reverse=True)
     topCCnumbers = sortedCC[:k] # CCs we assign scores to
     L = sum([l for (l,_) in topCCnumbers])
-    # add ties of rank k
+
     increment = 0
-    while sortedCC[k + increment][0] == sortedCC[k-1][0]:
+    # add ties of rank k (if len of kth CC == 1 then add all CCs)
+    while k+increment < len(sortedCC) and sortedCC[k + increment][0] == sortedCC[k-1][0]:
         topCCnumbers.append(sortedCC[k + increment])
         increment += 1
 
@@ -70,7 +70,6 @@ def CCWP(G, k, Ep):
         # weighted_score = 1
         for node in CC[numberCC]:
             scores[node] += weighted_score
-
     return scores
 
 def frange(begin, end, step):
@@ -100,38 +99,50 @@ if __name__ == '__main__':
     print time.time() - start
 
     #calculate initial set
-    seed_size = 60
     R = 500
     I = 250
-    fileno = 0
-
-    beste = -1
-    maxIS = -1
+    ftime = open('plotdata/timeDirectCCWPforDirect1_v2.txt', 'w+')
+    logfile = open('log.txt', 'w+')
+    print >>logfile, '--------------------------------'
+    print >>logfile, time.strftime("%c")
 
     # get propagation probabilities
-    Ep = random_from_range(G, [.01, .02, .04, .08])
+    Ep = dict()
+    with open("Ep_hep_range1.txt") as f:
+        for line in f:
+            data = line.split()
+            Ep[(int(data[0]), int(data[1]))] = float(data[2])
 
-    pool = pool2 = None
-    for e in frange(1,1.5,.5):
-        time2e = time.time()
-        print 'Start finding solution for e = %s' %e
+    length_to_coverage = {0:0}
+    l2c = [[0,0]]
+    # pool = pool2 = None
+    # pool = multiprocessing.Pool(processes=None, maxtasksperchild=1)
+    # pool2 = multiprocessing.Pool(processes=None, maxtasksperchild=1)
+
+    for length in range(1, 250, 5):
+        time2length = time.time()
+        print 'Start finding solution for length = %s' %length
+        print >>logfile, 'Start finding solution for length = %s' %length
+        time2S = time.time()
+
         print 'Start mapping...'
         time2map = time.time()
         # define map function
         def map_CCWP(it):
-            # print it
-            return CCWP(G, seed_size, Ep)
-        if pool == None:
-            pool = multiprocessing.Pool(processes=multiprocessing.cpu_count())
-        Scores = pool.map(map_CCWP, range(R))
+            return CCWP(G, length, Ep)
+        # if pool == None:
+        Scores = map(map_CCWP, range(R))
+        # pool.terminate()
+        # Scores = pool.apply(apply_CCWP, args=(length))
         print 'Finished mapping in', time.time() - time2map
-        time2reduce = time.time()
+
         print 'Reducing scores...'
+        time2reduce = time.time()
         scores = {v: sum([s[v] for s in Scores]) for v in G}
         scores_copied = deepcopy(scores)
         S = []
         # penalization phase
-        for it in range(seed_size):
+        for it in range(length):
             maxk, maxv = max(scores_copied.iteritems(), key = lambda (dk, dv): dv)
             # print maxv,
             S.append(maxk)
@@ -140,48 +151,35 @@ if __name__ == '__main__':
                 if v not in S:
                     # weight = scores_copied[v]/maxv
                     # print weight,
-                    penalty = (1-Ep[(maxk, v)])**(e*G[maxk][v]['weight'])
+                    penalty = (1-Ep[(maxk, v)])**(G[maxk][v]['weight'])
                     scores_copied[v] = penalty*scores_copied[v]
         print S
-        # topScores = nlargest(seed_size, scores.iteritems(), key = lambda (dk,dv): dv)
-        # S = [v for (v,_) in topScores]
-        def map_AvgIAC (it):
-            # print it
-            return avgIAC(G, S, Ep, I)
-        if pool2 == None:
-            pool2 = multiprocessing.Pool(processes=multiprocessing.cpu_count())
+        print >>logfile, json.dumps(S)
+        print >>ftime, "%s %s" %(length, time.time() - time2S)
         print 'Finished reducing in', time.time() - time2reduce
+
+        print 'Start calculating seed set size...'
+        def map_AvgIAC (it):
+            return avgIAC(G, S, Ep, I)
         avg_size = 0
         time2avg = time.time()
-        T = pool2.map(map_AvgIAC, range(4))
-        print 'Finished averaging seed set size in', time.time() - time2avg
+        T = map(map_AvgIAC, range(4))
         print T
         avg_size = sum(T)/len(T)
-        print 'Average size of %s nodes is %s' %(seed_size, avg_size)
+        print 'Average size of %s nodes is %s' %(length, avg_size)
+        print 'Finished averaging seed set size in', time.time() - time2avg
 
-        # write results to file
-        print 'Writing result to file...'
-        with open('../directCCWP/reverseCCWP%s.txt' %fileno, 'w+') as f:
-                f.write('e: ' + str(e) + ' k: ' + str(seed_size) + os.linesep)
-                f.write('S: ' + json.dumps(S) + os.linesep)
-                f.write('IS:' + str(avg_size))
-        fileno += 1
+        length_to_coverage[length] = avg_size
+        # l2c = sorted(length_to_coverage.iteritems(), key = lambda (dk, dv): dk)
+        l2c.append([length, avg_size])
+        with open("plotdata/plotDirectCCWPforDirect1_v2.txt", "w+") as fp:
+            json.dump(l2c, fp)
 
-        if avg_size >= maxIS:
-            print 'Changing best e to %s' %e
-            maxIS = avg_size
-            beste = e
-            bestS = S
-        print 'Total time for e = %s: %s sec' %(e, time.time() - time2e)
+        print 'Total time for length = %s: %s sec' %(length, time.time() - time2length)
         print '----------------------------------------------'
-        # delete existing variables
-        # pool = None
-        # pool2 = None
 
-    print 'maxIS:', maxIS
-    print 'beste:', beste
-    print 'bestS:', bestS
-    print '|S|:', len(S)
+    ftime.close()
+    logfile.close()
 
     print 'Total time: %s' %(time.time() - start)
 
