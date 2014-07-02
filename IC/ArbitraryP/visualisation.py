5901c39e018962622db9174ddf41f615f5ec13de
ivanovsergey <ivanovserg990@gmail.com>
2014-07-02 10:25:58 +0400

    Add plotCCsSizeDistribution

--- IC/ArbitraryP/visualisation.py
+++ IC/ArbitraryP/visualisation.py
@@ -48,27 +48,78 @@ def visualiseResults(x_lst, y_lst, filename="tempResults.png"):
         p, = plt.plot(x_lst[i], y_lst[i], colors[i] + 'o', markersize=10)
         plots.append(p)
 
-    plt.legend(plots, ["CCWP", "DD", "GDD"], loc=2)
+    plt.legend(plots, ["CCWP", "GDD", "PMIA"], loc=4)
     plt.grid()
     plt.xlabel('Seed set size')
     plt.ylabel('Influence spread')
-    plt.title('HepNEPT data. p = [.01, .02, .04, .08]')
+    # plt.title('HepNEPT data. p = [.01, .02, .04, .08]')
+    plt.title('HepNEPT -- Categories')
     fig.set_size_inches(18.5,10.5)
-
     fig.savefig(filename, dpi=fig.dpi)
+    # plt.show()
+
+def plotCCsSizeDistribution (histogram, bluedots, T, filename="plots/CCs_sizes.png",
+                             L = None, TotalCCs = None, model="Model",
+                             xlog = True, ylog = True):
+    '''
+     histogram: [(500, 1), (250, 1), ..., (6, 12), ..., (1, 1500)] -- [(size of CC, number of CCs of that size),...]
+     bluedots: number of qualified sizes
+     T -- Targeted spread
+     L -- minimum number of CCs that achieves T
+     TotalCCs -- total number of CCs
+     model -- name of Ep model (eg. MultiValency, Random, Uniform, WC, Categories)
+    '''
+
+    [x,y] = zip(*histogram)
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    if xlog:
+        ax.set_xscale("log")
+    if ylog:
+        ax.set_yscale("log")
+
+    p1 = plt.scatter(x[:bluedots],y[:bluedots], s=80, c="b")
+    p2 = plt.scatter(x[bluedots:],y[bluedots:], s=80, c="r")
+
+    x1,x2,y1,y2 = plt.axis()
+    plt.axis((1, x2, 10**(-1), y2))
+    plt.legend([p1, p2], ["Qualified", "Non-Qualified"], loc=1)
+    plt.xlabel('Size of Connected Components')
+    plt.ylabel('Number of Connected Components')
+    if L != None and TotalCCs != None:
+        plt.title('L = %s out of %s CCs' %(L, TotalCCs))
+    plt.text( 10**2, 10**4, '')
+    # plt.title('HepNEPT data. p <= .1')
+    fig.suptitle("HepNEPT -- %s. T = %s" %(model, T))
+    # fig.set_size_inches(18.5,10.5)
+    fig.savefig(filename, dpi=fig.dpi)
+
     # plt.show()
 
 
 if __name__ == "__main__":
-    print "Plotting results..."
-    DD_length_to_coverage = json.load(open("plotdata/plotDirectDDforDirect1.txt"))
-    CCWP_length_to_coverage = json.load(open("plotdata/plotDirectCCWPforDirect1_v2.txt"))
-    GDD_length_to_coverage = json.load(open("plotdata/plotDirectGDDforDirect1.txt"))
-    [DDx, DDy] = zip(*DD_length_to_coverage)
-    [CCWPx, CCWPy] = zip(*CCWP_length_to_coverage)
-    [GDDx, GDDy] = zip(*GDD_length_to_coverage)
+    # print "Plotting results..."
+    # # DD_length_to_coverage = json.load(open("plotdata/plotDirectDDforDirect5.txt"))
+    # CCWP_length_to_coverage = json.load(open("plotdata/plotDirectCCWPforDirect6.txt"))
+    # GDD_length_to_coverage = json.load(open("plotdata/plotDirectGDDforDirect6.txt"))
+    # PMIA_length_to_coverage = json.load(open("plotdata/plotDirectPMIAforDirect6.txt"))
+    # # [DDx, DDy] = zip(*DD_length_to_coverage)
+    # [CCWPx, CCWPy] = zip(*CCWP_length_to_coverage)
+    # [GDDx, GDDy] = zip(*GDD_length_to_coverage)
+    # [PMIAx, PMIAy] = zip(*PMIA_length_to_coverage)
+    #
+    # visualiseResults([CCWPx, GDDx, PMIAx], [CCWPy, GDDy, PMIAy], "plots/direct6.png")
 
-    visualiseResults([CCWPx, DDx, GDDx], [CCWPy, DDy, GDDy], "direct1_v3.png")
+    print "Plotting CCs sizes distribution..."
+    with open("plotdata/CCs_sizes_Multivalency1.txt") as fp:
+        bluedots = int(fp.readline())
+        T = int(fp.readline())
+        L = int(fp.readline())
+        TotalCCs = int(fp.readline())
+        histogram = json.loads(fp.readline())
+
+    model = "MultiValency"
+    plotCCsSizeDistribution(histogram, bluedots, T, "plots/CCs_sizes_multivalency_log.png", L, TotalCCs, model, xlog=True)
 
     # print "Plotting timing..."
     # CCWP_k = []
