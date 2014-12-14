4bac0943fe804908a06b8b5b85b95a9c063d295c
ivanovsergey <ivanovserg990@gmail.com>
2014-12-14 16:28:14 +0800

    remove edge weights

--- IC/ArbitraryP/PossibleWorlds.py
+++ IC/ArbitraryP/PossibleWorlds.py
@@ -7,8 +7,13 @@ http://www.francescobonchi.com/SIGMOD14-UG.pdf
 from __future__ import division
 import networkx as nx
 from itertools import cycle
-import random
+import random, copy, time
 from priorityQueue import PriorityQueue
+
+def avg(lst):
+    if len(lst):
+        return float(sum(lst))/len(lst)
+    return 0
 
 def GP (G, Ep):
     '''
@@ -18,6 +23,7 @@ def GP (G, Ep):
     if type(G) == type(nx.Graph()):
         expected_degree = dict(zip(G.nodes(), [0]*len(G)))
         for e in Ep:
+            # expected_degree[e[0]] += 1 - (1 - Ep[e])**G[e[0]][e[1]]["weight"]
             expected_degree[e[0]] += Ep[e]
 
         # initialize discrepancy
@@ -26,8 +32,9 @@ def GP (G, Ep):
             discrepancy[node] = -value
 
         live_edges = []
-        sorted_edges = sorted(Ep.iteritems(), key = lambda (e, p): p, reverse= True)
-        for e, _ in sorted_edges:
+        # sorted_edges = sorted(G.edges(), key = lambda e: 1 - (1 - Ep[e])**G[e[0]][e[1]]["weight"], reverse = True)
+        sorted_edges = sorted(G.edges(), key = lambda e: Ep[e], reverse = True)
+        for e in sorted_edges:
             u, v = e
             dis_u = discrepancy[u]
             dis_v = discrepancy[v]
@@ -35,17 +42,19 @@ def GP (G, Ep):
                 live_edges.append(e)
                 discrepancy[u] += 1
                 discrepancy[v] += 1
+        print 'Discrepancy after phase 1: %s' %sum(map(abs, discrepancy.values()))
+        print 'MAE: %s' %avg(map(abs, discrepancy.values()))
 
         E = nx.Graph()
         E.add_nodes_from(G)
-        E.add_edges_from(live_edges, weight=1)
+        E.add_edges_from(live_edges)
         return E
     elif type(G) == type(nx.DiGraph()):
         raise NotImplementedError
     else:
         raise NotImplementedError
 
-def ADR (G, Ep, steps):
+def ADR(G, Ep, steps):
     '''
     Average degree rewiring algorithm
     Note: do not consider multigraphs.
@@ -54,12 +63,14 @@ def ADR (G, Ep, steps):
         E_edges = dict()
         for node in G:
             E_edges[node] = []
-        P = round(sum(Ep.values()))
-        selected = dict(zip(Ep.keys(), [False]*len(Ep)))
+        # P = round(sum([1 - (1-Ep[e])**G[e[0]][e[1]]["weight"] for e in G.edges_iter()]))
+        P = round(sum([Ep[e] for e in G.edges_iter()]))
+        selected = dict(zip(G.edges(), [False]*len(G.edges())))
         number_of_edges = 0
 
         expected_degree = dict(zip(G.nodes(), [0]*len(G)))
         for e in Ep:
+            # expected_degree[e[0]] += 1 - (1 - Ep[e])**G[e[0]][e[1]]["weight"]
             expected_degree[e[0]] += Ep[e]
 
         # initialize discrepancy
@@ -67,11 +78,14 @@ def ADR (G, Ep, steps):
         for node, value in expected_degree.iteritems():
             discrepancy[node] = -value
 
-        sorted_edges = cycle(sorted(Ep.iteritems(), key = lambda (e, p): p, reverse = True))
+        # sorted_edges = cycle(sorted(G.edges(), key = lambda e: 1 - (1 - Ep[e])**G[e[0]][e[1]]["weight"], reverse = True))
+        sorted_edges = cycle(sorted(G.edges(), key = lambda e: Ep[e], reverse = True))
 
         # Phase 1: select first live edges
         while number_of_edges < P:
-            e, _ = sorted_edges.next()
+            # e, _ = sorted_edges.next()
+            e = sorted_edges.next()
+            # if not selected[e] and random.random() < 1 - (1 - Ep[e])**G[e[0]][e[1]]["weight"]:
             if not selected[e] and random.random() < Ep[e]:
                 E_edges[e[0]].append((e[0], e[1]))
                 E_edges[e[1]].append((e[1], e[0]))
@@ -80,6 +94,7 @@ def ADR (G, Ep, steps):
                 selected[e] = True
                 number_of_edges += 1
         blocked_edges = [e for e, value in selected.iteritems() if not value]
+        # print 'Discrepancy after phase 1: %s' %sum(map(abs, discrepancy.values()))
 
         # Phase 2: rewire live edges with blocked edges
         for it1 in range(steps):
@@ -106,20 +121,27 @@ def ADR (G, Ep, steps):
                     # update blocked edges
                     blocked_edges.append(e1)
                     blocked_edges.remove(e2)
+                    # update discrepancy
+                    discrepancy[e1[0]] -= 1
+                    discrepancy[e1[1]] -= 1
+                    discrepancy[e2[0]] += 1
+                    discrepancy[e2[1]] += 1
+        # print 'Discrepancy after phase 2: %s' %sum(map(abs, discrepancy.values()))
+        # print 'MAE: %s' %avg(map(abs, discrepancy.values()))
 
         selected_edges = []
         for edges_list in E_edges.values():
             selected_edges.extend(edges_list)
         E = nx.Graph()
         E.add_nodes_from(G)
-        E.add_edges_from(selected_edges, weight=1)
+        E.add_edges_from(selected_edges)
         return E
     elif type(G) == type(nx.DiGraph()):
         raise NotImplementedError
     else:
         raise NotImplementedError
 
-def bipartite(A, B, w, discrepancy):
+def bipartite(w, discrepancy):
     Ebp_edges = []
     Q = PriorityQueue()
     incident_edges = dict()
@@ -130,23 +152,28 @@ def bipartite(A, B, w, discrepancy):
     processed_edges = []
     while len(processed_edges) < len(w):
         (e, weight) = Q.pop_item()
-        # print e
         processed_edges.append(e)
-        incident_edges[e[0]].remove((e[1], -weight))
-        incident_edges[e[1]].remove((e[0], -weight))
+        try:
+            incident_edges[e[0]].remove((e[1], -weight))
+            incident_edges[e[1]].remove((e[0], -weight))
+        except ValueError:
+            pass
         Ebp_edges.append(e)
 
         # discard all edges in Q incident to b (i.e. e[1])
-        # print e[1], incident_edges[e[1]]
         for (a, weight) in incident_edges[e[1]]:
             try:
                 Q.remove_task((a,e[1]))
                 processed_edges.append((a,e[1]))
             except KeyError:
                 pass
-            incident_edges[a].remove((e[1], weight))
-            incident_edges[e[1]].remove((a, weight))
+            try:
+                incident_edges[a].remove((e[1], weight))
+                incident_edges[e[1]].remove((a, weight))
+            except ValueError:
+                pass
         discrepancy[e[0]] += 1
+        discrepancy[e[1]] += 1
 
         if -1 < discrepancy[e[0]] < .5:
             for (x, _) in incident_edges[e[0]]:
@@ -168,12 +195,12 @@ def bipartite(A, B, w, discrepancy):
                     pass
     return Ebp_edges
 
-
 def ABM(G, Ep):
     if type(G) == type(nx.Graph()):
         # calculate expected degree
         expected_degree = dict(zip(G.nodes(), [0]*len(G)))
         for e in Ep:
+            # expected_degree[e[0]] += 1 - (1 - Ep[e])**G[e[0]][e[1]]["weight"]
             expected_degree[e[0]] += Ep[e]
 
         current_degree = dict(zip(G.nodes(), [0]*len(G)))
@@ -185,7 +212,11 @@ def ABM(G, Ep):
         for node in expected_degree:
             b[node] = round(expected_degree[node])
 
-        for e in G.edges():
+        unsorted_edges = G.edges()
+        shuffled_idxs = range(len(G.edges()))
+        random.shuffle(shuffled_idxs)
+        shuffled_edges = [unsorted_edges[idx] for idx in shuffled_idxs]
+        for e in shuffled_edges:
             if current_degree[e[0]] < b[e[0]] and current_degree[e[1]] < b[e[1]]:
                 Em_edges.append(e)
                 current_degree[e[0]] += 1
@@ -206,6 +237,7 @@ def ABM(G, Ep):
                 B.append(u)
             else:
                 C.append(u)
+        # print 'Discrepancy after phase 1: %s' %sum(map(abs, discrepancy.values()))
 
         w = dict()
         for e in Eprime_edges:
@@ -215,14 +247,15 @@ def ABM(G, Ep):
 
         Gprime = nx.Graph()
         Gprime.add_weighted_edges_from(map(lambda ((u, v), weight): (u,v,weight), w.items()))
-        Ebp_edges = bipartite(A, B, w, discrepancy)
+        Ebp_edges = bipartite(w, discrepancy)
+        # print 'Discrepancy after phase 2: %s' %sum(map(abs, discrepancy.values()))
+        # print 'MAE: %s' %avg(map(abs, discrepancy.values()))
 
         E = nx.Graph()
         E.add_nodes_from(G)
-        E.add_edges_from(Em_edges + Ebp_edges, weight=1)
+        E.add_edges_from(Em_edges + Ebp_edges)
         return E
     elif type(G) == type(nx.DiGraph()):
         raise NotImplementedError
     else:
         raise NotImplementedError
-
