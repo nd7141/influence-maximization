407492106d3a5925487decd19629958ea3f5c0d8
ivanovsergey <ivanovserg990@gmail.com>
2015-01-26 19:10:11 +0800

    Add random sparcification method

--- IC/ArbitraryP/MPST.py
+++ IC/ArbitraryP/MPST.py
@@ -4,16 +4,13 @@ Extract MPST and delete edges from original graph that belong to that MPST.
 Continue until all edges are deleted from G.
 Use those MPST to select a possible world (PW) and approximate reliability.
 
-How do we compute ground-truth?
-How do we compute reliability for PW?
-
-We are solving 1st problem: minimizing sum over all pairs of reliability with a constraint on the number of edges
+We are solving sparsification problem of uncertain graphs for preserving reliability.
 '''
 from __future__ import division
 import networkx as nx
 from itertools import cycle
 from math import exp, log
-import random, time, sys
+import random, time, sys, json
 from collections import Counter
 from itertools import product
 
@@ -115,14 +112,15 @@ def get_rel_with_mpst(MPSTs, pairs=None):
     '''
     rel = dict()
     if pairs:
+        print_dict = dict()
         for idx, pair in enumerate(pairs):
             u = pair[0]
             v = pair[1]
             for idx, mpst in enumerate(MPSTs):
                 r = find_reliability_in_mpst(mpst, u, v)
-                print idx, len(MPSTs), r
                 rel[(u,v)] = rel.get((u,v), 0) + r
                 rel[(v,u)] = rel.get((v,u), 0) + r
+                print_dict[pair] = rel[pair]
     else:
         for en, mpst in enumerate(MPSTs):
             print 'mpst #', en, "len(mpst):", len(mpst)
@@ -134,7 +132,10 @@ def get_rel_with_mpst(MPSTs, pairs=None):
                     r = find_reliability_in_mpst(mpst, u, v)
                     rel[(u,v)] = rel.get((u,v), 0) + r
                     rel[(v,u)] = rel.get((v,u), 0) + r
-    print sorted(rel.items(), key = lambda (k,v): v, reverse=True)[:10]
+    if pairs:
+        print sorted(print_dict.items(), key = lambda (_,v): v, reverse = True)[:10]
+    else:
+        print sorted(rel.items(), key = lambda (_,v): v, reverse=True)[:10]
     return rel
 
 def get_rel_with_mc(G, mc=100, pairs=None):
@@ -156,15 +157,17 @@ def get_rel_with_mc(G, mc=100, pairs=None):
         # print "%s: Created a random PW" %(_+1)
         # print 'E:', len(E), 'E.edges:', len(E.edges())
         if pairs:
+            print_dict = dict()
             # for every provided pair of nodes check if there is a path
             for pair in pairs:
                 u = pair[0]
                 v = pair[1]
                 r = 0
-                if pair in live_edges:
+                if u in E and v in E and nx.has_path(E, u, v):
                     r = 1./mc
                 rel[(u,v)] = rel.get((u,v), 0) + r
                 rel[(v,u)] = rel.get((v,u), 0) + r
+                print_dict[pair] = rel[pair]
         else:
             # for every pair of nodes in E check if there is a path
             CCs = nx.connected_components(E)
@@ -173,7 +176,10 @@ def get_rel_with_mc(G, mc=100, pairs=None):
                     for u in cc:
                         for v in cc:
                             rel[(u,v)] = rel.get((u,v), 0) + 1./mc
-    print sorted(rel.items(), key = lambda (k,v): v, reverse=True)[:10]
+    if pairs:
+        print sorted(print_dict.items(), key = lambda (k,v): v, reverse = True)[:10]
+    else:
+        print sorted(rel.items(), key = lambda (k,v): v, reverse=True)[:10]
     return rel
 
 def get_rel_for_pw(PW, pairs=None):
@@ -185,6 +191,7 @@ def get_rel_for_pw(PW, pairs=None):
     all_edges = PW.edges()
     rel = dict()
     if pairs:
+        print_dict = dict()
         for pair in pairs:
             u = pair[0]
             v = pair[1]
@@ -193,12 +200,16 @@ def get_rel_for_pw(PW, pairs=None):
                 r = 1
             rel[(u,v)] = r
             rel[(v,u)] = r
+            print_dict[pair] = rel[pair]
     else:
         CCs = nx.connected_components(PW)
         for cc in CCs:
             cc_pairs = list(product(cc, repeat=2))
             rel.update(dict(zip(cc_pairs, [1]*len(cc_pairs))))
-    print sorted(rel.items(), key = lambda (k,v): v, reverse=True)[:10]
+    if pairs:
+        print sorted(print_dict.items(), key = lambda (k,v): v, reverse = True)[:10]
+    else:
+        print sorted(rel.items(), key = lambda (k,v): v, reverse=True)[:10]
     return rel
 
 def get_objective(G_rel, PW_rel):
@@ -226,9 +237,9 @@ def _make_pairs(G, l):
             pairs.append((u,v))
     return pairs
 
-def get_sparcified_mpst(MPSTs, K):
+def get_sparsified_mpst(MPSTs, K):
     '''
-    Get sparcified (uncetain graph with K edges) graph using MPST.
+    Get sparsified (uncertain graph with K edges) graph using MPST.
 
     MPSTs ordered from largest to smallest.
 
@@ -246,20 +257,22 @@ def get_sparcified_mpst(MPSTs, K):
 
     return SP
 
-def get_sparcified_greedy(G, K):
+def get_sparsified_greedy(G, K):
     '''
-    Get sparcified (uncetain graph with K edges) graph using greedy.
+    Get sparsified (uncertain graph with K edges) graph using greedy.
 
     Expected to have -log(p_e) as weights in G.
     '''
     all_edges = G.edges(data=True)
     sorted_edges = sorted(all_edges, key = lambda (u,v,data): exp(1)**(-data["weight"]), reverse=True)
+    print 'sorted edges...'
     # create a SP
     selected = dict()
     for (u,v,d) in all_edges:
         selected[(u,v)] = False
         selected[(v,u)] = False
     SP = nx.Graph()
+    progress = 1
     for (u,v,data) in cycle(sorted_edges):
         # add edge with probability p
         if not selected[(u,v)] and exp(1)**(-data["weight"]) > random.random():
@@ -268,13 +281,16 @@ def get_sparcified_greedy(G, K):
             selected[(v,u)] = True
             # print "Added edge (%s,%s): P %s |PW.edges| %s" %(u,v,P,len(PW.edges()))
         # stop when expected number of edges reached
+        if len(SP.edges()) == int(progress*.1*K):
+            progress += 1
+            print '%s%% processed...' %(progress*10)
         if len(SP.edges()) == K:
             break
     return SP
 
-def get_sparcified_top(G,K):
+def get_sparsified_top(G, K):
     '''
-    Get sparcified (uncetain graph with K edges) graph using top most probable edges.
+    Get sparsified (uncertain graph with K edges) graph using top most probable edges.
 
     Expected to have -log(p_e) as weights in G.
     '''
@@ -284,6 +300,17 @@ def get_sparcified_top(G,K):
     SP.add_edges_from(sorted_edges[:K])
     return SP
 
+def get_sparsified_random(G, K):
+    '''
+    Get sparsified (uncertain graph with K edges) graph using random edges.
+
+    Expected to have -log(p_e) as weights in G.
+    '''
+    all_edges = G.edges(data=True)
+    random_edges = random.sample(all_edges, K)
+    SP = nx.Graph() # sparcified graph
+    SP.add_edges_from(random_edges[:K])
+    return SP
 
 if __name__ == "__main__":
     time2execute = time.time()
@@ -303,41 +330,102 @@ if __name__ == "__main__":
     l = 4000
     # pairs = _make_pairs(G, l)
     pairs = [(11052, 11371), (9097, 10655)]
-    print pairs
+    pairs = pairs[0][:1]
+    pairs = G.edges()
     print 'Made %s pairs in %s sec' %(l, time.time() - time2pairs)
     print
 
-    time2PW = time.time()
-    PW, MPSTs = get_PW(G)
-    print "Extracted MPST PW in %s sec" %(time.time() - time2PW)
-    print
+    # Get PW, MPST, SP
+    # time2PW = time.time()
+    # PW, MPSTs = get_PW(G)
+    # print "Extracted MPST PW in %s sec" %(time.time() - time2PW)
+    # print
 
-    time2Grel2 = time.time()
-    G_rel2 = get_rel_with_mpst(MPSTs, pairs)
-    print "Calculated G MPST reliability in %s sec" %(time.time() - time2Grel2)
-    print
+    P = sum([exp(1)**(-data["weight"]) for (u,v,data) in G.edges(data=True)]) # expected number of edges
+    percentage = .1
+    obj_results = dict()
+    for track in range(1, 11):
+        print '------------------------------------'
+        print 'Step %s' %track
+        K = track*int(percentage*len(G.edges()))
+        track += 1
+        # K = 5*int(P)
+        print 'K:', K, "|G|:", len(G.edges())
 
-    time2Grel1 = time.time()
-    G_rel1 = get_rel_with_mc(G, 100, pairs)
-    print "Calculated G MC reliability  in %s sec" %(time.time() - time2Grel1)
-    print
+        # time2SP1 = time.time()
+        # SP1 = get_sparsified_mpst(MPSTs, K)
+        # print "Extracted MPST SP in %s sec" %(time.time() - time2SP1)
+        # print
 
-    time2PWrel = time.time()
-    PW_rel = get_rel_for_pw(PW, pairs)
-    print "Calculated PW reliability in %s sec" %(time.time() - time2PWrel)
-    print
+        # time2SP2 = time.time()
+        # SP2 = get_sparsified_greedy(G, K)
+        # print "Extracted Greedy SP in %s sec" %(time.time() - time2SP2)
+        # print
 
-    time2Obj1 = time.time()
-    Obj1 = get_objective(G_rel1, PW_rel)
-    print "Calculated MC-MC objective in %s sec" %(time.time() - time2Obj1)
-    print "MC-MC Objective: %s" %Obj1
-    print
+        # time2SP3 = time.time()
+        # SP3 = get_sparsified_top(G, K)
+        # print "Extracted Top SP in %s sec" %(time.time() - time2SP3)
+        # print
 
-    time2Obj2 = time.time()
-    Obj2 = get_objective(G_rel2, PW_rel)
-    print "Calculated MPST-MC objective in %s sec" %(time.time() - time2Obj2)
-    print "MPST-MC Objective: %s" %Obj2
-    print
+        time2SP_Random = time.time()
+        SP_random = get_sparsified_random(G, K)
+        print "Extracted Random SP in %s sec" %(time.time() - time2SP_Random)
+        print
+
+        # ----------------- Get Reliability ------------------ #
+        MC = 100
+
+        time2Grel1 = time.time()
+        G_rel1 = get_rel_with_mc(G, MC, pairs)
+        print "Calculated G MC reliability  in %s sec" %(time.time() - time2Grel1)
+        print
+
+        time2SP_Randomrel = time.time()
+        SP_random_rel = get_rel_with_mc(SP_random, MC, pairs)
+        print "Calculated SP MC reliability  in %s sec" %(time.time() - time2SP_Randomrel)
+        print
+
+
+        # time2Grel2 = time.time()
+        # G_rel2 = get_rel_with_mpst(MPSTs, pairs)
+        # print "Calculated G MPST reliability in %s sec" %(time.time() - time2Grel2)
+        # print
+        #
+        # time2PWrel = time.time()
+        # PW_rel = get_rel_for_pw(PW, pairs)
+        # print "Calculated PW reliability in %s sec" %(time.time() - time2PWrel)
+        # print
+
+        # time2SP1rel = time.time()
+        # SP1_rel = get_rel_with_mc(SP1, MC, pairs)
+        # print "Calculated MPST SP MC reliability  in %s sec" %(time.time() - time2SP1rel)
+        # print
+
+        # time2SP2rel = time.time()
+        # SP2_rel = get_rel_with_mc(SP2, MC, pairs)
+        # print "Calculated Greedy SP MC reliability  in %s sec" %(time.time() - time2SP2rel)
+        # print
+
+        # time2SP3rel = time.time()
+        # SP3_rel = get_rel_with_mc(SP3, MC, pairs)
+        # print "Calculated Top SP MC reliability  in %s sec" %(time.time() - time2SP3rel)
+        # print
+
+        # ---------------------- Calculate Objective -------------------------- #
+        time2Obj1 = time.time()
+        Obj1 = get_objective(G_rel1, SP_random_rel)
+        print "Calculated MC-MC objective in %s sec" %(time.time() - time2Obj1)
+        print "MC-MC Objective: %s" %Obj1
+        print
+        obj_results.update({(track-1)*10: Obj1})
+        with open("Sparsified_results/Sparsified_Random_MC%s.txt" %MC, "w+") as fp:
+            json.dump(obj_results, fp)
+        #
+        # time2Obj2 = time.time()
+        # Obj2 = get_objective(G_rel2, PW_rel)
+        # print "Calculated MPST-MC objective in %s sec" %(time.time() - time2Obj2)
+        # print "MPST-MC Objective: %s" %Obj2
+        # print
 
     print "Finished execution in %s sec" %(time.time() - time2execute)
 
